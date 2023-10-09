5c055ee1663fbae304c9d575ef4ad6235658f102
Matheus <ymatheus.h@gmail.com>
2023-10-09 17:54:51 -0300

    feat: :wrench: create theme file

---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    GREEN_DARK: '#00875F',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GRAY_700: '#1B1D1E',
+    GRAY_600: '#333638',
+    GRAY_500: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_300: '#DDDEDF',
+    GRAY_200: '#EFF0F0',
+    GRAY_100: '#FAFAFA',
+    WHITE: '#FFFFFF',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
+  }
+};
