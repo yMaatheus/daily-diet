8bd6040216b18224cf7737a47b07da1252bfeef3
Matheus <ymatheus.h@gmail.com>
2023-10-18 01:31:21 -0300

    feat: :sparkles: implements app font loader nunito sans

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from '@expo-google-fonts/nunito-sans';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
+import { Loading } from '@components/Loading';
+import { Home } from '@screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        style="auto"
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
