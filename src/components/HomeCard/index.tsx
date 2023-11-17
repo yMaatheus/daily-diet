e54b0c917c8681a0722d4c3994b890c20b415bdd
Matheus <ymatheus.h@gmail.com>
2023-11-17 19:44:38 -0300

    feat: :sparkles: create component HomeCard

---
+++ src/components/HomeCard/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+export function HomeCard() {
+  return (
+    <S.Container>
+      <S.Title>90,86%</S.Title>
+      <S.Description>das refeições dentro da dieta</S.Description>
+      <S.Icon />
+    </S.Container>
+  )
+}
