b81095411e5ab479285923be2e72f15a92d5462a
Matheus <ymatheus.h@gmail.com>
2023-11-18 04:01:33 -0300

    refactor: :label: refactor statistics route types

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,7 +2,11 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      statistics: undefined;
+      statistics: {
+        percentage: string;
+        data: ItemMealList[];
+        isDiet: boolean;
+      };
     }
   }
 }
