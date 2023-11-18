588a6a0e8b925671f3a49928d2999b215b526500
Matheus <ymatheus.h@gmail.com>
2023-11-17 23:45:39 -0300

    style: :coffin: remove dead code

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -10,9 +10,3 @@ export const Screen = styled.SafeAreaView`
 export const HomeContainer = styled.View`
   gap: 32px;
 `;
-
-export const Title = styled.Text`
-  color: ${({ theme }) => theme.COLORS.GRAY_700};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-`;
