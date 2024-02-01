00823d94e9cca512416db3f830cb8de37889b55f
Matheus <ymatheus.h@gmail.com>
2024-02-01 17:17:06 -0300

    style: :coffin: remove dead code

--- src/storage/create-meal.ts
+++ src/storage/create-meal.ts
@@ -2,15 +2,12 @@ import { ItemMealList } from "@/data";
 import { Meal } from "@/types/meal";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { compareDesc, format, parse } from "date-fns";
-import { addStreakDiet } from "./add-streak-diet";
 import { getAllMeals } from "./get-all-meals";
-import { resetStreakDiet } from "./reset-streak-diet";
 import { MEALS_COLLECTION } from "./storageConfig";
 
 export async function createMeal(title: string, meal: Meal) {
   title = format(parse(title, "dd/MM/yyyy", new Date()), "dd.MM.yy");
   
-  await streak(meal.isDiet);
   const meals = await getAllMeals();
 
   const findItemTitle = meals.find((item) => item.title === title);
@@ -41,12 +38,4 @@ async function setMeals(meals: ItemMealList[]) {
 
   const storage = JSON.stringify(array);
   await AsyncStorage.setItem(MEALS_COLLECTION, storage);
-}
-
-async function streak(isDiet: boolean) {
-  if (isDiet) {
-    await addStreakDiet();
-  } else {
-    await resetStreakDiet();
-  }
 }
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,1 @@
 export const MEALS_COLLECTION = '@ignite-daily-diet:meals';
-
-export const DIET_STREAK_COLLECTION = '@ignite-daily-diet:diet-streak';
